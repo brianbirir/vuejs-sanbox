e97ed3b17a37716a7bc1ea30bb77ed27796f6a7b
Brian Birir <brianbirir@gmail.com>
2018-11-01 23:03:51 +0300

    add new item form

--- kanban/src/components/Backlog.vue
+++ kanban/src/components/Backlog.vue
@@ -1,11 +1,21 @@
 <template>
   <div class="backlog-view">
     <p>Backlog</p>
+    <new-item></new-item>
   </div>
 </template>
 
 <script>
+
+import { mapState } from 'vuex'
+
+import NewItemForm from './NewItemForm'
+
 export default {
-  name: 'Backlog'
+  name: 'Backlog',
+  components: {
+    'new-item': NewItemForm
+  }
 }
+
 </script>
